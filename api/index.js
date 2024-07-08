6cce3547579a7f713e89c7f45e44eccbb4142c26
ameya-6964 <belvalkar7@gmail.com>
2024-07-09 03:16:57 +0530

    Added Error Handling To API

--- api/index.js
+++ api/index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", function (req, res) {
-res.json({message:"Hello"})
+app.get("/", function (req, res, next) {
+    try {
+        res.send('Hey');
+    } catch (err) {
+        next(err)
+    }
 })
+
+
+// ERROR HANDLING
+app.use((err,req,res,next) =>{
+    res.status(500).send(err.message)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server Started On Port ${PORT}`);
