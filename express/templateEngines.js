71301669cd272c38a24062ca5d9aac7d0e099a44
ameya-6964 <belvalkar7@gmail.com>
2024-07-09 02:37:28 +0530

    Understood Form Handling With POST Method

--- express/templateEngines.js
+++ express/templateEngines.js
@@ -3,14 +3,22 @@ const path = require("path")
 const app = express();
 const PORT = 3000;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-   res.render('index')
+    res.render('index')
+})
+
+app.post("/check", function (req, res) {
+    console.log(req.body);
+    let name = req.body.name;
+    res.render("working", { name });
 })
 
 app.get("/profile", function (req, res) {
-   res.render('profile')
+    res.render('profile')
 })
 
 app.listen(PORT, () => {
