8c55b8be1f2b9d8bc025b315e8bf9d99e2326c90
ameya-6964 <belvalkar7@gmail.com>
2024-07-09 21:32:58 +0530

    Completed CRUD Operation With MongoDB

--- crud-operations/index.js
+++ crud-operations/index.js
@@ -11,6 +11,41 @@ app.get("/", function (req, res) {
     res.send("Hey")
 })
 
+app.get("/create", async function (req, res) {
+    let user = await userModel.create({
+        username: "ameya-6964",
+        name: "Ameya Belvalkar",
+        email: "ameya@gmail.com",
+        password: "123456"
+    })
+    res.send(user);
+})
+
+
+app.get("/read", async function (req, res) {
+    let user = await userModel.find()
+    res.send(user);
+})
+
+app.get("/read/:id", async function (req, res) {
+    let id = req.params.id
+    let user = await userModel.findOne({ _id: id })
+    res.send(user);
+})
+
+
+app.get("/update/:id", async function (req, res) {
+    let id = req.params.id
+    let user = await userModel.findOneAndUpdate({ _id: id },{username:"ameya-7977"} ,{new:true})
+    res.send(user);
+})
+
+app.get("/delete/:id", async function (req, res) {
+    let id = req.params.id
+    let user = await userModel.findOneAndDelete({ _id: id })
+    res.send(user);
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server Started On Port ${PORT}`);
