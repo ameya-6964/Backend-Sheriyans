6819b1791298c4081f8062b172448f0437c2b89a
ameya-6964 <belvalkar7@gmail.com>
2024-07-09 15:07:38 +0530

    Added Route To Add Task

--- mini_project-1/index.js
+++ mini_project-1/index.js
@@ -12,7 +12,23 @@ app.set("view engine", "ejs")
 
 
 app.get("/", function (req, res) {
-    res.render("index")
+    fs.readdir(`./files`, function (err, files) {
+        if (err) {
+            return res.send("ERROR");
+        }
+        res.render('index', { files: files })
+    })
+})
+
+app.get('/create', function (req, res) {
+    res.render("create")
+})
+
+app.post('/createtask', function (req, res) {
+    fs.writeFile(`./files/${req.body.title}.txt`, req.body.content, (err) => {
+        if (err) return res.status(500).send(err);
+        res.redirect("/")
+    })
 })
 
 app.listen(PORT, () => {
