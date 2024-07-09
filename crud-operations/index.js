7581fd1906cd0dd21b4ae7f684f4525c31b81f23
ameya-6964 <belvalkar7@gmail.com>
2024-07-09 21:07:04 +0530

    Created Seperation Of Concerns For Database Connection And Models

--- crud-operations/index.js
+++ crud-operations/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express();
 const userModel = require('./models/userModel')
+const db = require('./config/db')
 const PORT = 3000;
+
+// Connecting To Local Database
+db();
 
 app.get("/", function (req, res) {
     res.send("Hey")
--- crud-operations/models/userModel.js
+++ crud-operations/models/userModel.js
@@ -1,6 +1,5 @@
-const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://127.0.0.1:27017/crud-demo`)
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     name: String,
