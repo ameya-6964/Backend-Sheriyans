8ed2154eb3642d8a605e44a63d28c0e736e8a9b4
ameya-6964 <belvalkar7@gmail.com>
2024-07-11 16:30:38 +0530

    Created Neccesary Folder Structure

--- jwt/index.js
+++ jwt/index.js
@@ -1,23 +1,20 @@
+// Imports
+require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const PORT = 3000;
+const connectDb = require("./config/db")
 
+//Constants
+const PORT = process.env.PORT;
 const app = express();
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.cookie("age", "25", {
-        maxAge: 2000,
-    })
-    res.send("Cookie Set")
-})
-
-app.get("/readcookie", (req, res) => {
-    let { age } = req.cookies;
-    res.send(age)
-})
+// Connecting To Database
+connectDb();
 
 app.listen(PORT, () => {
     console.log(`Server Started On Port 3000`);
